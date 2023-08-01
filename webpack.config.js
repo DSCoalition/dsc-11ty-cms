5bd9c41b80eb436aa62fc1099408bf1b6ea78c3f
SeaJerry <jsea@kindsys.us>
2023-08-01 13:23:19 -0700

    Change CSS/webpack output path to _site/styles & add DSC assets

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: './src/scripts/main.js',
   output: {
-    path: path.resolve(__dirname, '_site/assets'),
+    path: path.resolve(__dirname, '_site/styles'),
     filename: 'main.js'
   },
   plugins: [new MiniCssExtractPlugin()],
