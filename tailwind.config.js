022ca224a65f9c9d2f9176e2b0f0f7969a10212c
SeaJerry <jsea@kindsys.us>
2023-08-24 13:34:19 -0700

    Add extend object

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,23 +5,25 @@ module.exports = {
     "./src/index.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
   ],
   theme: {
-    colors: {
-      red: "#ec193f",
-      pink: "#d21b67",
-      blue: "#394da0",
-      purple: "#6f52a1",
-      magenta: "#92268e",
-      gray: "#444444",
-      grayLight: "#777777",
-      grayLightest: "#f9f7f6",
-      white: "#ffffff",
-      greenLight: "#D4EDDA",
-      greenDark: "#155724"
-    },
-    screens: {
-      tablet: "768px",
-      desktop: "992px",
-      xl: "1200px",
+    extend: {
+      colors: {
+        red: "#ec193f",
+        pink: "#d21b67",
+        blue: "#394da0",
+        purple: "#6f52a1",
+        magenta: "#92268e",
+        gray: "#444444",
+        grayLight: "#777777",
+        grayLightest: "#f9f7f6",
+        white: "#ffffff",
+        greenLight: "#D4EDDA",
+        greenDark: "#155724"
+      },
+      screens: {
+        tablet: "768px",
+        desktop: "992px",
+        xl: "1200px",
+      },
     },
   },
 };
